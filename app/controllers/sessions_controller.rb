6a90e14bebd31a2118070c9a0fa38265ef0d9a0b
Rachel Kawula <rkawula@gmail.com>
2015-11-25 01:26:30 -0800

    Signing up and logging in now work. Logging out is next.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
+
+  end
+
+  def create
+  	user = User.find_by_username params[:session][:username].downcase
+  	if user && user.authenticate(params[:session][:password])
+  		log_in user
+  		redirect_to profile_path(user.username)
+  	else
+  		flash.now[:danger] = 'Invalid email/password.'
+  		render 'new'
+  	end
+  end
+
+  def destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,15 +11,20 @@ Rails.application.routes.draw do
   get 'resume', to: 'welcome#resume'
 
   # User accounts:
-  # log in
-  get 'login', to: 'user#login'
   # sign up
   get 'signup', to: 'user#new'
   # make new user
   post 'signup', to: 'user#create'
   # profile
-  get 'profile/:username', to: 'user#show'
-  
+  get 'profile/:username', to: 'user#show', as: 'profile'
   # Later, add editing and deletion of profiles (PATCH, DELETE)
+
+  # Sessions:
+  # log in
+  get 'login', to: 'sessions#new'
+  # begin new session
+  post 'login', to: 'sessions#create'
+  # log out
+  delete 'logout', to: 'sessions#destroy'
 
 end
