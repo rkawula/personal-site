c76a9103f37c799dcba0b5e65f44beba573a8791
Rachel Kawula <rkawula@gmail.com>
2015-11-24 21:46:30 -0800

    Added admin to users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119052341) do
+ActiveRecord::Schema.define(version: 20151125054242) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -25,9 +25,10 @@ ActiveRecord::Schema.define(version: 20151119052341) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
