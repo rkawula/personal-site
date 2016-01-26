c8fd85d197785dfece409ce0c7a823d855f27f0d
Rachel Kawula <rkawula@gmail.com>
2016-01-26 10:40:09 -0800

    Added section for projects.

---
+++ features/projects.feature
@@ -0,0 +1,7 @@
+Feature: Projects
+
+Scenario: Clickable link to Projects
+
+Given that I am at the home page
+When I click "Projects"
+Then I should be at the "Projects" webpage
---
+++ features/step_definitions/projects.rb
@@ -0,0 +1,7 @@
+Given(/^that I am at the home page$/) do
+  visit root_path
+end
+
+Then(/^I should be at the "(.*?)" webpage$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
